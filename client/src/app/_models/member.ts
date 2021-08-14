3688795ab67cd34224d19e8b7e1bcd3af3f1e3d7
abrahamassembly@gmail.com <abrahamassembly@gmail.com>
2021-08-14 14:46:27 -0500

    added functional neighbors tab to see other users

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,14 @@
+
+export interface Member {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    knownAs: string;
+    age: number;
+    created: Date;
+    lastActive: Date;
+    introduction: string;
+    interests: string;
+    city: string;
+    country: string;
+}
