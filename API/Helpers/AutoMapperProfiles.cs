ba70ff2533df101fe7b155bb7a5f20f6d6cbb4be
abrahamassembly@gmail.com <abrahamassembly@gmail.com>
2021-08-14 13:22:32 -0500

    Implemented AutoMapper for user/member

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+using API.DTO;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDTO>().ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+        }
+    }
+}
